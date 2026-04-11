3b171fb1e836006306957a6ec79f832176e40f7c
AnantLad <anantlad018@gmail.com>
2026-04-11 17:22:06 +0530

    Day-4 of learning terraform

--- Projects/02-project/main.tf
+++ Projects/02-project/main.tf
@@ -9,3 +9,14 @@ resource "aws_instance" "anant" {
 resource "aws_s3_bucket" "s3_bucket" {
     bucket = "anant-s3-bucket-01"
 }
+resource "aws_dynamodb_table" "dynamodb_table_1" {
+  
+    name         = "anant-dynamodb-table-01"
+    billing_mode = "PAY_PER_REQUEST"
+    hash_key     = "id"
+    
+    attribute {
+        name = "id"
+        type = "S"
+    }
+    }
