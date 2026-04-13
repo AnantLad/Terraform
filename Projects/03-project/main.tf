c6c721f85bde69dad562e7dc67e679380e5456ab
AnantLad <anantlad018@gmail.com>
2026-04-13 09:56:28 +0530

    Day 5 of learning terraform

--- Projects/03-project/main.tf
+++ Projects/03-project/main.tf
@@ -58,6 +58,14 @@ resource "aws_security_group" "sg" {
         protocol    = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
+    ingress {
+        description = "Allow HTTPS"
+        from_port   = 443
+        to_port     = 443
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
     egress {
         description = "Allow all outbound traffic"
         from_port   = 0
